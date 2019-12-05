fc827c105db52d1ac8ccf63c9e19a97d6b377601
Bonny69 <47884873+Bonny69@users.noreply.github.com>
2019-12-05 10:35:27 +0100

    creata la repository con la soluzione

---
+++ 10-progetto finale/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace _10_progetto_finale
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Image main = new Image();
+        DispatcherTimer shoot = new DispatcherTimer(), enemy = new DispatcherTimer();
+        Random rand = new Random();
+        Point point = new Point();
+        double height = 0, width;
+        int angolo = 0;
+        bool pause = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            shoot.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            shoot.Tick += Shoot_Tick;
+        }
+        private void Shoot_Tick(object sender, EventArgs e)
+        {
+            for (int i = canvas.Children.Count - 1; i > 0; i--)
+            {
+                if (Canvas.GetLeft(canvas.Children[i]) > ((canvas.ActualWidth / 2) + 25))
+                    Canvas.SetLeft(canvas.Children[i], Canvas.GetLeft(canvas.Children[i]) + 10);
+                else
+                {
+                    if (Canvas.GetLeft(canvas.Children[i]) < ((canvas.ActualWidth / 2) - 25))
+                    {
+                        Canvas.SetLeft(canvas.Children[i], Canvas.GetLeft(canvas.Children[i]) - 10);
+                    }
+                }
+                Canvas.SetTop(canvas.Children[i], Canvas.GetTop(canvas.Children[i]) - 10);
+                if (Canvas.GetTop(canvas.Children[i]) <= -25)
+                {
+                    canvas.Children.RemoveAt(i);
+                }
+            }
+            if (canvas.Children.Count - 1 == 0)
+                shoot.Stop();
+            else
+                shoot.Start();
+        }
+        private void GameStart(object sender, MouseButtonEventArgs e)
+        {
+            if(point.Equals(new Point()))
+            {
+                start_screen.Visibility = Visibility.Hidden;
+                point = new Point((canvas.ActualWidth / 2), (canvas.ActualHeight / 2));
+            }
+            else
+            {
+                Shoot();
+            }
+            //vita1.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/cuore_vuoto.png"));
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(!point.Equals(new Point()))
+            {
+
+                if (!pause)
+                {
+                    switch (e.Key)
+                    {
+                        case Key.D:
+                            Muovi(10);
+                            break;
+                        case Key.Right:
+                            Muovi(10);
+                            break;
+                        case Key.A:
+                            Muovi(-10);
+                            break;
+                        case Key.Left:
+                            Muovi(-10);
+                            break;
+                        case Key.Space:
+                            Shoot();
+                            break;
+                        case Key.Escape:
+                            shoot.Stop();
+                            enemy.Stop();
+                            pause_screen.Visibility = Visibility.Visible;
+                            pause = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    if (e.Key.Equals(Key.Escape))
+                    {
+                        shoot.Start();
+                        //enemy.Start();
+                        pause_screen.Visibility = Visibility.Hidden;
+                        pause = false;
+                    }
+                }
+            }
+        }
+        void Muovi(int movimento)
+        {
+            angolo += movimento;
+            switch (angolo)
+            {
+                case 360:
+                    angolo = 0;
+                    break;
+                case -360:
+                    angolo = 0;
+                    break;
+            }
+            RotateTransform rotated = new RotateTransform();
+            rotated.Angle = angolo;
+            main.RenderTransformOrigin = new Point(0.5, 0.5);
+            main.RenderTransform = rotated;
+        }
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            double cat1 = e.GetPosition(this).Y - point.Y;
+            double ipotenusa = Math.Sqrt(Math.Pow((e.GetPosition(this).X - point.X), 2) + Math.Pow(cat1, 2));
+            double sin = cat1 / ipotenusa;
+            //sin /= (ipotenusa / 10);
+            //cos /= (ipotenusa / 10);
+            double pino = Math.Pow(Math.Sin(sin), -1);
+            temp.Text = point.X.ToString() + "|" + point.Y + "; " + e.GetPosition(this).X + "|" + e.GetPosition(this).Y+"\n"+cat1+":"+ipotenusa+"\n"+ (int)Math.Asin(sin)+"|"+angolo+"|"+(int)pino+"\n"+sin;
+            Muovi((int)pino);
+        }
+        void Shoot()
+        {
+            Ellipse palla = new Ellipse();
+            palla.Width = 20;
+            palla.Height = 50;
+            palla.Fill = Brushes.Red;
+            double angolo_matematico = ((angolo-70) * -1) * System.Math.PI / 180;
+            Canvas.SetTop(palla, Canvas.GetTop(main) - 50 + ((main.Height + 25) * (1 - Math.Sin(angolo_matematico))));
+            Canvas.SetLeft(palla, Canvas.GetLeft(main) + ((main.Height + 25) * Math.Cos(angolo_matematico)));
+            canvas.Children.Add(palla);
+            shoot.Start();
+        }
+        private void Resize(object sender, SizeChangedEventArgs e)
+        {
+            for (int i = 0; i < canvas.Children.Count; i++)
+            {
+                Canvas.SetTop(canvas.Children[i], (canvas.ActualHeight * Canvas.GetTop(canvas.Children[i])) / height);
+                Canvas.SetLeft(canvas.Children[i], (canvas.ActualWidth * Canvas.GetLeft(canvas.Children[i])) / width);
+                (canvas.Children[i] as Image).Height = (canvas.ActualHeight * (canvas.Children[i] as Image).Height) / height;
+                (canvas.Children[i] as Image).Width = (canvas.ActualWidth * (canvas.Children[i] as Image).Width) / width;
+            }
+            if(!height.Equals(0))
+            {
+                score.FontSize = (canvas.ActualHeight * score.FontSize) / height;
+                score.Width = (canvas.ActualHeight * score.Width) / height;
+                start.FontSize = (canvas.ActualHeight * start.FontSize) / height;
+                start.Width = (canvas.ActualHeight * start.Width) / height;
+            }
+            height = canvas.ActualHeight;
+            width = canvas.ActualWidth;
+        }
+        private void Canvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            main.Height = canvas.ActualHeight / 3.5;
+            main.Width = (396 * main.Height) / 500;
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri("pack://application:,,,/Resources/main.png");
+            img.EndInit();
+            main.Source = img;
+            canvas.Children.Add(main);
+            Canvas.SetTop(main, (canvas.ActualHeight / 2) - (main.Height / 2));
+            Canvas.SetLeft(main, (canvas.ActualWidth / 2) - (main.Height / 2));
+            //Canvas.SetTop(main, canvas.ActualHeight - main.Height);
+            //Canvas.SetLeft(main, 0);
+        }
+    }
+}
